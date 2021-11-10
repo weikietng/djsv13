f0179317f8372ee01e7872c288a155ca47c71c9a
djng314 <superpianist.ng@gmail.com>
2021-11-10 20:20:29 +0000

    a

--- index.js
+++ index.js
@@ -73,37 +73,24 @@ var client = new discord_js_1.default.Client({
     ],
 });
 client.on('ready', function () { return __awaiter(void 0, void 0, void 0, function () {
-    var dbOptions, wok, currentUser;
+    var wok, mongoose, db, currentUser;
     var _a;
     return __generator(this, function (_b) {
         switch (_b.label) {
             case 0:
-                dbOptions = {
-                    // These are the default values
-                    keepAlive: true
-                };
                 wok = new wokcommands_1.default(client, {
                     // The name of the local folder for your command files
                     commandsDir: path_1.default.join(__dirname, 'commands'),
                     // Pass in the new dbOptions
-                    dbOptions: dbOptions,
                     // Pass in your own mongo connection URI
-                    mongoUri: "mongodb+srv://cerezadiscord:Cereza314@cluster0.1gt3c.mongodb.net/test"
                 });
-                wok.on('databaseConnected', function (connection, state) { return __awaiter(void 0, void 0, void 0, function () {
-                    var model, results;
-                    return __generator(this, function (_a) {
-                        switch (_a.label) {
-                            case 0:
-                                model = connection.models['Account'];
-                                return [4 /*yield*/, model.countDocuments()];
-                            case 1:
-                                results = _a.sent();
-                                console.log(results);
-                                return [2 /*return*/];
-                        }
-                    });
-                }); });
+                mongoose = require('mongoose');
+                mongoose.connect(process.env.dbLink);
+                db = mongoose.connection;
+                db.on("error", console.error.bind(console, "connection error:"));
+                db.once("open", function () {
+                    console.log("Connection To MongoDB Atlas Successful!");
+                });
                 (_a = client.user) === null || _a === void 0 ? void 0 : _a.setPresence({ activities: [{ name: 'Cereza | Under Development', type: "LISTENING" }], status: 'idle' });
                 return [4 /*yield*/, noblox_js_1.default.setCookie("_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_D817227E322191D066D8C166C88A91200129B012596761D526B43333EC125D379EA7722DCE2DD3998CD9B075049C61E87410665AA73CED82E18DC1B562AE9EA939634149B9ED574358671A28C3E1EC71047DF17237B1F614F0A2C386D8D55A678F48D5DEF6AD8940521AA1E6B8056159E69D4F6577DEA5187177EB66287A5C7B03196BF614543116AF50B8E1A6BAA4845248F4A9544A1501930B619785ED62163E36A95DF70F110A09C0860EDEEE6DDAB338C9B7B444462FDCB46D9CC81B1DD9EEDCAACDBE13A1741C4C225EE856B7C32FE3535928B701E20FE33409AE89B0DE77171EE22E256FB81F315F372187971439CEB58AB8ABA2BA88CEE09CF5EC12AAE8C93EF5DED9DEA34FEA76A14DF8A2F72E37EE518920A074D76A065D97F41FAAAD8B60D48FEFEBF815008289D2770FB9F4EF5BA063D271E62D832B1B119DE5FE3EE159780AD881BE01E0E86748D3EB998364C2E1")];
             case 1:
