a1be2b891a0dac6cf7605ecab7c01846985c31db
djng314 <superpianist.ng@gmail.com>
2021-11-10 20:42:54 +0000

    a

--- commands/profile.js
+++ commands/profile.js
@@ -4,8 +4,14 @@ var verification = require("../models/account");
 exports.default = {
     category: 'Utility',
     description: 'Return with user profile',
+    slash: "both",
     callback: function (_a) {
-        var message = _a.message;
-        message.reply(message.author.id + " testing");
+        var message = _a.message, interaction = _a.interaction;
+        if (message) {
+            message.reply(message.author.username + " testing");
+        }
+        if (interaction) {
+            interaction.reply(interaction.user.username + " testing");
+        }
     }
 };
--- commands/profile.ts
+++ commands/profile.ts
@@ -6,11 +6,16 @@ const verification = require("../models/account");
 export default{
     category: 'Utility',
     description: 'Return with user profile',
-    
+    slash: "both",
 
 
-    callback:({message})=>{
-      message.reply(`${message.author.id} testing`)
+    callback:({message,interaction})=>{
+        if (message){
+            message.reply(`${message.author.username} testing`)
+        }
+      if(interaction){
+        interaction.reply(`${interaction.user.username} testing`)
+      }
         
 
     }
